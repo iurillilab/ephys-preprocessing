2f68179e7e3dce35867fb147d640768ffee4b815
Rorsharck <yadunandanet@gmail.com>
2025-03-25 16:44:08 +0100

    Updated code with kilosort

--- scripts/yadu/filtering_trace.py
+++ scripts/yadu/filtering_trace.py
@@ -160,15 +160,55 @@ plt.tight_layout()
 plt.show()
 
 # %%
-kilosort
-
-# %%
 import kilosort
 # %%
 from kilosort import run_kilosort
 # %%
-from kilosort import run_kilosort, DEFAULT_SETTINGS
+def call_ks(preprocessed_recording, stream_name, working_folder_path, callKSfromSI=True, remove_binary=True):
+    working_folder_path = Path(working_folder_path)
+    
+    DTYPE = np.int16
+    
+    probe = preprocessed_recording.get_probe()
 
+    if callKSfromSI: #takes forever even with latest spikeinterface version. stopped it.
+        # call kilosort from here, without saving the .bin (will have to call something else to run phy)
+        print("Starting KS4")
+        t_start = time.perf_counter()
+        sorting_ks4_prop = ss.run_sorter_by_property("kilosort4", recording=preprocessed_recording, 
+                        grouping_property="group", working_folder=working_folder_path, verbose=True)
+        t_stop = time.perf_counter()
+        elapsed_prop = np.round(t_stop - t_start, 2)
+        print(f"Elapsed time by property: {elapsed_prop} s")
+
+    else:
+        # kilosort export .bin continuous data (this can be read by phy too)
+        # haven't tried the wrapper, pessimistic about the time it takes
+        probename = "probe_{}.prb".format(stream_name)
+        filename, N, c, s, fs, probe_path = io.spikeinterface_to_binary(
+            preprocessed_recording, working_folder_path, data_name='data.bin', dtype=DTYPE,
+            chunksize=60000, export_probe=True, probe_name=probename
+            )
+        
+        #run KS programmatically
+        settings = {'fs': fs, 'n_chan_bin': probe.get_contact_count()}
+        probe_dict = io.load_probe(probe_path)
+        kilosort_optional_params = {}
+        if torch is not None:
+            kilosort_optional_params["device"] = torch.device("cuda")
+
+        ops, st_ks, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = run_kilosort(
+                            settings=settings, probe=probe_dict, filename=filename, data_dtype=DTYPE, **kilosort_optional_params
+        )
+
+        # decide whether you want to delete data.bin afterwards
+        bin_path = working_folder_path / 'data.bin'
+        if bin_path.exists() and remove_binary:
+            bin_path.unlink()
+
+#%%
+call_ks(rec, "neuropixPhase3B1_kilosortChanMap.mat", working_folder_path=r'Y:\20250124\M20\test_npx1\2025-01-24_19-56-04\Record Node 102\experiment1\recording1\continuous\Neuropix-PXI-100.ProbeB-AP\kilosort4')
+# %%
 settings = DEFAULT_SETTINGS
 # ( path to drive if mounted: /content/drive/MyDrive/ )
 settings['data_dir'] = r'Y:\20250124\M20\test_npx1\2025-01-24_19-56-04\Record Node 102\experiment1\recording1\continuous\Neuropix-PXI-100.ProbeB-AP'
@@ -195,8 +235,7 @@ clu = np.load(results_dir / 'spike_clusters.npy')
 firing_rates = np.unique(clu, return_counts=True)[1] * 30000 / st.max()
 dshift = ops['dshift']
 # %%
-%matplotlib inline
-import matplotlib.pyplot as plt
+
 from matplotlib import gridspec, rcParams
 rcParams['axes.spines.top'] = False
 rcParams['axes.spines.right'] = False
@@ -250,4 +289,43 @@ for k in range(2):
         ax.set_yscale('log')
         ax.set_title('loglog')
 
+# %%
+probe = ops['probe']
+# x and y position of probe sites
+xc, yc = probe['xc'], probe['yc']
+nc = 16 # number of channels to show
+good_units = np.nonzero(contam_pct <= 0.1)[0]
+mua_units = np.nonzero(contam_pct > 0.1)[0]
+
+
+gstr = ['good', 'mua']
+for j in range(2):
+    print(f'~~~~~~~~~~~~~~ {gstr[j]} units ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('title = number of spikes from each unit')
+    units = good_units if j==0 else mua_units
+    fig = plt.figure(figsize=(12,3), dpi=150)
+    grid = gridspec.GridSpec(2,20, figure=fig, hspace=0.25, wspace=0.5)
+
+    for k in range(40):
+        wi = units[np.random.randint(len(units))]
+        wv = templates[wi].copy()
+        cb = chan_best[wi]
+        nsp = (clu==wi).sum()
+
+        ax = fig.add_subplot(grid[k//20, k%20])
+        n_chan = wv.shape[-1]
+        ic0 = max(0, cb-nc//2)
+        ic1 = min(n_chan, cb+nc//2)
+        wv = wv[:, ic0:ic1]
+        x0, y0 = xc[ic0:ic1], yc[ic0:ic1]
+
+        amp = 4
+        for ii, (xi,yi) in enumerate(zip(x0,y0)):
+            t = np.arange(-wv.shape[0]//2,wv.shape[0]//2,1,'float32')
+            t /= wv.shape[0] / 20
+            ax.plot(xi + t, yi + wv[:,ii]*amp, lw=0.5, color='k')
+
+        ax.set_title(f'{nsp}', fontsize='small')
+        ax.axis('off')
+    plt.show()
 # %%
