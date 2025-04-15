53ab322ead1068b93feb1b36c2cb3bde990708d8
Rorsharck <yadunandanet@gmail.com>
2025-04-15 12:07:26 +0200

    nwb file update

--- scripts/yadu/nwb_conversion.py
+++ scripts/yadu/nwb_conversion.py
@@ -1,10 +1,13 @@
 #%%
+%matplotlib widget
 from datetime import datetime
+import matplotlib.pyplot as plt
 from zoneinfo import ZoneInfo
 from pathlib import Path
 import numpy as np
 
 from neuroconv.datainterfaces import KiloSortSortingInterface
+print('hello world')
 #%%
 
 folder_path = r'Y:\20250124\M20\test_npx1\2025-01-24_19-56-04\Record Node 102\experiment1\recording1\continuous\Neuropix-PXI-100.ProbeB-AP\kilosort4\sorter_output'
@@ -24,16 +27,19 @@ data.key_to_id
 # %%
 spikes = data["units"]
 spikes
+# %%
+dir(spikes)
 #%%
-z = spikes['original_cluster_id']
-tmp = { 0:nap.Ts(t=np.arange(0,200), time_units='s'),1:nap.Ts(t=np.arange(0,200,0.5), time_units='s'),2:nap.Ts(t=np.arange(0,300,0.25), time_units='s'),}
+a = spikes.to_tsd()
+spike_times = np.array(a)
+unit_id = spikes["unit_name"].to_numpy()
+spike_times
+#%%
+plt.plot(a.index.to_numpy(), a.to_numpy(), 'o')
+plt.xlabel('Spike Time (s)')
+plt.ylabel('Unit ID')
+plt.show()
 
-tsgroup = nap.TsGroup(tmp)
-
-ep = nap.IntervalSet(start=0, end=100, time_units='s')
-
-bincount = tsgroup.count(0.1, ep)
-
-bincount
-
+# %%
+b = spikes.to_tsd('unit_name')
 # %%
