68e5bb119bc164e96698156b8cd53bd3605d2a2f
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-06-09 16:28:24 +0200

    started cleanedup script

--- scripts/luigi/multi-day/check_spike_times.py
+++ scripts/luigi/multi-day/check_spike_times.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 import spikeinterface.curation as scur
 import pynapple as nap
+import shutil
 
 from neuroconv.datainterfaces import KiloSortSortingInterface, OpenEphysRecordingInterface
 # %%
@@ -42,10 +43,10 @@ print(len(timestamps), timestamps.min(), timestamps.max())
 # %%
 
 sample_folder = Path("/Users/vigji/Desktop/07_PREY_HUNTING_YE/e01_ephys _recordings/M29_WT002/20250509/113126")  # Path("/Users/vigji/Desktop/short_recording_oneshank/2025-01-22_16-56-15")
-folder_path = sample_folder / "kilosort4"
+ks_folder_path = sample_folder / "kilosort4"
 
 
-def dirty_nwb_like_loder(folder_path: Path) -> pd.DataFrame:
+def dirty_nwb_like_loder(folder_path: Path, overwrite: bool = False) -> pd.DataFrame:
     # Change the folder_path to the location of the data in your system
     try:
         recording_folder = next((folder_path.parent / "NPXData").glob("2025-*"))
@@ -62,9 +63,13 @@ def dirty_nwb_like_loder(folder_path: Path) -> pd.DataFrame:
 
     nwb_path = folder_path.parent / "sorter_output.nwb"
 
-    ## if nwb_path.exists():
-        #return nap.load_file(nwb_path)
-    recording_extractor.get_times
+    if nwb_path.exists():
+        if overwrite:
+            nwb_path.unlink()
+        else: 
+            return nap.load_file(nwb_path)
+    times = recording_extractor.get_times()
+    print(f"Times shape: {times.shape}, max: {np.max(times)}, min: {np.min(times)}")
     ks_interface = KiloSortSortingInterface(folder_path=folder_path / "sorter_output", 
                                          verbose=False,
                                          keep_good_only=False)
@@ -80,4 +85,30 @@ def dirty_nwb_like_loder(folder_path: Path) -> pd.DataFrame:
 #sorter = read_kilosort(sample_folder / "kilosort4" / "sorter_output", keep_good_only=False)
 # %%
 # units_df = load_units_df(sample_folder / "kilosort4")
-nwb_file = dirty_nwb_like_loder(sample_folder / "kilosort4")
+nwb_file = dirty_nwb_like_loder(ks_folder_path, overwrite=True)
+
+# Filter good units
+good_units_mask =  (nwb_file["units"]["KSLabel"] == "good") 
+
+good_units = nwb_file["units"][good_units_mask]
+print(f"Total units: {len(nwb_file['units'])}")
+print(f"Good units: {len(good_units)}")
+
+# Convert TsGroup to Tsd
+good_units_tsd = good_units.to_tsd()
+print(f"Converted to Tsd with shape: {good_units_tsd.shape}")
+print("First spike time: ", np.min(good_units_tsd.t), "Last spike time: ", np.max(good_units_tsd.t))
+
+
+# Calculate spike counts for each unit
+bin_size = 1
+firing_rates = good_units.count(bin_size=bin_size).d / bin_size
+zsc_firing_rates = (firing_rates - firing_rates.mean(axis=0)) / firing_rates.std(axis=0)
+print(f"Z-scored firing rates shape: {zsc_firing_rates.shape}")
+first_bin = np.nansum(firing_rates[0, :])
+last_bin = np.nansum(firing_rates[-1, :])
+print(f"First bin: {first_bin}, Last bin: {last_bin}")
+
+# %%
+
+# %%
